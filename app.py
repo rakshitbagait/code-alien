1d16aca72e4c554c34061093bb458e0b72703fbb
rakshitbagait <rbagait2018@gmail.com>
2025-10-20 23:55:02 +0530

    updated app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 
 
 app = Flask(__name__)
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug =True)
