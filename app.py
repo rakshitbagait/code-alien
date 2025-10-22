c734a47c6210048f9f9b93dee64e4326b3ca2dca
rakshitbagait <rbagait2018@gmail.com>
2025-10-22 21:41:30 +0530

    final app

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template,url_for,request
 from flask_mail import Mail,Message
 import os 
 from dotenv import load_dotenv
+import mysql.connector
+import markdown
 load_dotenv()
 app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -11,9 +13,28 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MY_EMAIL')  # your email
 app.config['MAIL_PASSWORD'] = os.getenv('MY_PASSWORD')  # your app password
 mail = Mail(app)
+
+def get_db_connection():
+    c_alien_db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database="code_alien_db",
+        port=3306,
+        connection_timeout=5,
+        use_pure = True
+    )
+    return c_alien_db
 @app.route("/")
 def home():
-    return render_template('index.html')
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT blog_id, blog_title, image FROM blog ORDER BY date_of_publish DESC")    
+    blog =cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html',blogs=blog)
+
 @app.route("/about")
 def about():
     return render_template("about.html")
@@ -41,5 +62,17 @@ def send_email():
             return render_template("contact.html", message="Something went wrong. Please try again.")
     
     return render_template("contact.html")
+
+@app.route("/blog/<int:blog_id>")
+def show_blog(blog_id):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM blog WHERE blog_id=%s", (blog_id,))
+    blog = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if blog and 'blog_description' in blog:
+        blog['blog_description'] = markdown.markdown(blog['blog_description'])
+    return render_template("template.html", blog=blog)
 if __name__ == "__main__":
     app.run(debug =True)
